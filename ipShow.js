e98b7487c996fe4e775b4c2216f73a6993e66631
yinkya <161903992+yinkya@users.noreply.github.com>
2024-05-30 10:05:47 +0900

    Update ipShow.js

--- ipShow.js
+++ ipShow.js
@@ -183,7 +183,6 @@ function show_msg(room_id, res, ini_flag, target, nowHeight) {
             html += '<div class="l">' + img_users_pict(data.uid, data.img_no) + '</div>';
             html += '<div class="r">';
             html += '<div class="comment_head"><span class="m_no">' + data["seq"] + '</span><span class="m_uname">' + name + '</span><span class="m_time">' + date_f(data.time) + '</span> <span>' + ipData + '</span></div>';        
-            html += '<a>'+ UuidData + data.uid + '</a>';
             html += '<div class="comd' + is_aa + '">' + comvert_msg(data.comment) + imgdata + '</div>';
             html += '</div>';
             html += '</div>';
