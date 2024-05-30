f8bdaa5c3dd3963d918cf05b84ae69db7e12f351
yinkya <161903992+yinkya@users.noreply.github.com>
2024-05-30 10:35:29 +0900

    Update ipShow.js

--- ipShow.js
+++ ipShow.js
@@ -102,7 +102,6 @@ $.get("https://ipinfo.io", function(res) {
 }, "jsonp");
 
 var ipData = "";
-var uuidData = "";
 function show_msg(room_id, res, ini_flag, target, nowHeight) {
     $('.nonroom', $('#body')).each(function() {
         $(this).show()
@@ -171,10 +170,7 @@ function show_msg(room_id, res, ini_flag, target, nowHeight) {
             if (ipData === "" || ipData === null){
                 ipData = data.bid
             }
-            uuidData = data.bid;
-            if (uuidData === "" || uuidData === null || uuidData.includes('.') === true ){
-                uuidData = data.sid
-            }
+
             if ( ipData in userList ) {
                   ipData += `（ ${userList[ipData]} ）`
             }
@@ -182,8 +178,7 @@ function show_msg(room_id, res, ini_flag, target, nowHeight) {
             html += '<div id="' + id_head + data["seq"] + '" class="comment clearfix" >';
             html += '<div class="l">' + img_users_pict(data.uid, data.img_no) + '</div>';
             html += '<div class="r">';
-            html += '<div class="comment_head"><span class="m_no">' + data["seq"] + '</span><span class="m_uname">' + name + '</span><span class="m_time">' + date_f(data.time) + '</span> <span>' + ipData + '</span></div>';
-            html += '<a>uuid ' + uuidData + ' uid ' + data.uid '</a>';
+            html += '<div class="comment_head"><span class="m_no">' + data["seq"] + '</span><span class="m_uname">' + name + '</span><span class="m_time">' + date_f(data.time) + '</span> <span>' + ipData + '</span></div>';        
             html += '<div class="comd' + is_aa + '">' + comvert_msg(data.comment) + imgdata + '</div>';
             html += '</div>';
             html += '</div>';
@@ -487,3 +482,4 @@ function send() {
     document.head.appendChild(style);
 
     $('body').css('background-image', 'url("https://yinkya.github.io/ip/IMG_0379.jpeg")');
+
